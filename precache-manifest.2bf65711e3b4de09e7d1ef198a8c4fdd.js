7ae243816a842302b90770ce6529f85385fd9b00
Артём Изосимов <1996loko@rambler.ru>
2019-02-03 03:33:28 +0300

    Make gh pages deployment

---
+++ precache-manifest.2bf65711e3b4de09e7d1ef198a8c4fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14aebb6cc03939ae9d69",
+    "url": "/static/css/main.e8cfbc39.chunk.css"
+  },
+  {
+    "revision": "14aebb6cc03939ae9d69",
+    "url": "/static/js/main.14aebb6c.chunk.js"
+  },
+  {
+    "revision": "05a5deec86427ef8f2e6",
+    "url": "/static/js/1.05a5deec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3121ad4c06130ad4e255c3318cde883b",
+    "url": "/index.html"
+  }
+];
